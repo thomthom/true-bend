5b18a90d6989a7fede0c088d4d71eaa871db3159
Thomas Thomassen <thomas@thomthom.net>
2019-02-11 22:29:18 +0100

    Fix Layout/IndentationWidth

--- src/tt_truebend/dpi.rb
+++ src/tt_truebend/dpi.rb
@@ -4,159 +4,159 @@ require 'tt_truebend/dpi/view'
 require 'tt_truebend/app_settings'
 
 module TT::Plugins::TrueBend
-# Utility module for computing unit conversions based on monitor DPI.
-module DPI
+  # Utility module for computing unit conversions based on monitor DPI.
+  module DPI
 
-  # TODO: Deduplicate
-  SKETCHUP_VERSION = Sketchup.version.to_i
+    # TODO: Deduplicate
+    SKETCHUP_VERSION = Sketchup.version.to_i
 
-  # To test scaling on a "normal" DPI monitor, use this method to force
-  # scaling. SketchUp needs to be restarted for it to take full effect.
-  #
-  #     TT::Plugins::TrueBend::DPI.debug_scale(2.0)
-  #     TT::Plugins::TrueBend::DPI.debug_scale(nil)
-  #
-  # @param [Float, nil] scale
-  def self.debug_scale(scale)
-    if scale
-      SETTINGS.debug_force_high_dpi = true
-      set_scaling(scale)
-    else
-      SETTINGS.debug_force_high_dpi = false
-    end
-  end
-
-  def self.debug_scale?
-    SETTINGS.debug_force_high_dpi?
-  end
-
-  # Scaling Units
-
-  # Sets the manual scaling used for older SketchUp versions that doesn't have
-  # UI.scale_factor.
-  #
-  # @param [Float, nil] scale
-  def self.set_scaling(scale) # rubocop:disable Naming/AccessorMethodName
-    @tr_scale_to_device = nil
-    @tr_scale_to_logical = nil
-    SETTINGS.dpi_scale_factor = scale
-  end
-
-  if SETTINGS.debug_force_high_dpi?
-    # @return [Float]
-    def self.scale_factor
-      SETTINGS.dpi_scale_factor
-    end
-  elsif UI.respond_to?(:scale_factor)
-    # @return [Float]
-    def self.scale_factor
-      UI.scale_factor
-    end
-  else
-    # @return [Float]
-    def self.scale_factor
-      SETTINGS.dpi_scale_factor
-    end
-  end
-
-  # @param [Numeric] logical_unit
-  # @return [Float]
-  def self.to_device(logical_unit)
-    logical_unit * scale_factor
-  end
-
-  # @param [Numeric] device_unit
-  # @return [Float]
-  def self.to_logical(device_unit)
-    device_unit / scale_factor
-  end
-
-  # Converts from device pixels to logical pixels.
-  #
-  # @param [Float] x
-  # @param [Float] y
-  # @return [Array(Float, Float)]
-  def self.logical_pixels(x, y)
-    s = DPI.scale_factor
-    [x / s, y / s]
-  end
-
-  # Converts from logical pixels to device pixels.
-  #
-  # @param [Float] x
-  # @param [Float] y
-  # @return [Array(Float, Float)]
-  def self.device_pixels(x, y)
-    s = DPI.scale_factor
-    [x * s, y * s]
-  end
-
-  # Transformation the scales from logical units to device units.
-  #
-  # @return [Geom::Transformation]
-  def self.scale_to_device_transform
-    @tr_scale_to_device ||= Geom::Transformation.scaling(scale_factor,
-        scale_factor,
-        scale_factor)
-    @tr_scale_to_device
-  end
-
-  # Transformation the scales from device units to logical units.
-  #
-  # @return [Geom::Transformation]
-  def self.scale_to_logical_transform
-    @tr_scale_to_logical ||= scale_to_device_transform.inverse
-    @tr_scale_to_logical
-  end
-
-  # Scaling line width
-
-  if UI.respond_to?(:scale_factor)
-    def self.force_scale_factor?
-      SETTINGS.debug_force_high_dpi? && UI.scale_factor == 1.0
-    end
-  else
-    def self.force_scale_factor?
-      SETTINGS.debug_force_high_dpi?
-    end
-  end
-
-  if self.force_scale_factor?
-    # @param [Numeric]
-    # @return [Numeric]
-    def self.scale_line_width(width)
-      to_device(width)
-    end
-  else
-    # @param [Numeric] width
-    # @return [Numeric]
-    def self.scale_line_width(width)
-      if SKETCHUP_VERSION > 16
-        # SketchUp 17 scales line weights automatically.
-        width
+    # To test scaling on a "normal" DPI monitor, use this method to force
+    # scaling. SketchUp needs to be restarted for it to take full effect.
+    #
+    #     TT::Plugins::TrueBend::DPI.debug_scale(2.0)
+    #     TT::Plugins::TrueBend::DPI.debug_scale(nil)
+    #
+    # @param [Float, nil] scale
+    def self.debug_scale(scale)
+      if scale
+        SETTINGS.debug_force_high_dpi = true
+        set_scaling(scale)
       else
-        to_device(width)
+        SETTINGS.debug_force_high_dpi = false
       end
     end
-  end
 
-  # Scaling web dialogs
-
-  # SketchUp 2017 scales up the position and size automatically for both
-  # UI::WebDialog and UI::HtmlDialog.
-  if SKETCHUP_VERSION > 16
-    # @param [Numeric] logical_pixels
-    # @return [Numeric]
-    def self.scale_webdialog(logical_pixels)
-      logical_pixels
+    def self.debug_scale?
+      SETTINGS.debug_force_high_dpi?
     end
-  else
-    # @param [Numeric] logical_pixels
-    # @return [Numeric]
-    def self.scale_webdialog(logical_pixels)
-      to_device(logical_pixels)
-    end
-  end
 
-end
+    # Scaling Units
+
+    # Sets the manual scaling used for older SketchUp versions that doesn't have
+    # UI.scale_factor.
+    #
+    # @param [Float, nil] scale
+    def self.set_scaling(scale) # rubocop:disable Naming/AccessorMethodName
+      @tr_scale_to_device = nil
+      @tr_scale_to_logical = nil
+      SETTINGS.dpi_scale_factor = scale
+    end
+
+    if SETTINGS.debug_force_high_dpi?
+      # @return [Float]
+      def self.scale_factor
+        SETTINGS.dpi_scale_factor
+      end
+    elsif UI.respond_to?(:scale_factor)
+      # @return [Float]
+      def self.scale_factor
+        UI.scale_factor
+      end
+    else
+      # @return [Float]
+      def self.scale_factor
+        SETTINGS.dpi_scale_factor
+      end
+    end
+
+    # @param [Numeric] logical_unit
+    # @return [Float]
+    def self.to_device(logical_unit)
+      logical_unit * scale_factor
+    end
+
+    # @param [Numeric] device_unit
+    # @return [Float]
+    def self.to_logical(device_unit)
+      device_unit / scale_factor
+    end
+
+    # Converts from device pixels to logical pixels.
+    #
+    # @param [Float] x
+    # @param [Float] y
+    # @return [Array(Float, Float)]
+    def self.logical_pixels(x, y)
+      s = DPI.scale_factor
+      [x / s, y / s]
+    end
+
+    # Converts from logical pixels to device pixels.
+    #
+    # @param [Float] x
+    # @param [Float] y
+    # @return [Array(Float, Float)]
+    def self.device_pixels(x, y)
+      s = DPI.scale_factor
+      [x * s, y * s]
+    end
+
+    # Transformation the scales from logical units to device units.
+    #
+    # @return [Geom::Transformation]
+    def self.scale_to_device_transform
+      @tr_scale_to_device ||= Geom::Transformation.scaling(scale_factor,
+          scale_factor,
+          scale_factor)
+      @tr_scale_to_device
+    end
+
+    # Transformation the scales from device units to logical units.
+    #
+    # @return [Geom::Transformation]
+    def self.scale_to_logical_transform
+      @tr_scale_to_logical ||= scale_to_device_transform.inverse
+      @tr_scale_to_logical
+    end
+
+    # Scaling line width
+
+    if UI.respond_to?(:scale_factor)
+      def self.force_scale_factor?
+        SETTINGS.debug_force_high_dpi? && UI.scale_factor == 1.0
+      end
+    else
+      def self.force_scale_factor?
+        SETTINGS.debug_force_high_dpi?
+      end
+    end
+
+    if self.force_scale_factor?
+      # @param [Numeric]
+      # @return [Numeric]
+      def self.scale_line_width(width)
+        to_device(width)
+      end
+    else
+      # @param [Numeric] width
+      # @return [Numeric]
+      def self.scale_line_width(width)
+        if SKETCHUP_VERSION > 16
+          # SketchUp 17 scales line weights automatically.
+          width
+        else
+          to_device(width)
+        end
+      end
+    end
+
+    # Scaling web dialogs
+
+    # SketchUp 2017 scales up the position and size automatically for both
+    # UI::WebDialog and UI::HtmlDialog.
+    if SKETCHUP_VERSION > 16
+      # @param [Numeric] logical_pixels
+      # @return [Numeric]
+      def self.scale_webdialog(logical_pixels)
+        logical_pixels
+      end
+    else
+      # @param [Numeric] logical_pixels
+      # @return [Numeric]
+      def self.scale_webdialog(logical_pixels)
+        to_device(logical_pixels)
+      end
+    end
+
+  end
 end # module
--- src/tt_truebend/dpi/inputpoint.rb
+++ src/tt_truebend/dpi/inputpoint.rb
@@ -5,57 +5,57 @@ require 'tt_truebend/dpi/view'
 require 'tt_truebend/dpi'
 
 module TT::Plugins::TrueBend
-# Shim that takes care of dealing monitor DPI.
-#
-# The users of this class work with logical units and they are automatically
-# converted to device pixels for the API to consume where appropriate.
-#
-# @note Not possible to make this a mix-in module because Sketchup::InputPoint
-#   check the type by strict checks instead of an is_a? check.
-class HighDpiInputPoint < SimpleDelegator
-
-  # @overload initialize
+  # Shim that takes care of dealing monitor DPI.
   #
-  # @overload initialize(inputpoint)
-  #   @param [Sketchup::InputPoint, HighDpiInputPoint] inputpoint
-  def initialize(*args)
-    ip = if args.empty?
-      Sketchup::InputPoint.new
-    elsif args.first.is_a?(Sketchup::InputPoint)
-      args.first
-    else
-      Sketchup::InputPoint.new(*args)
+  # The users of this class work with logical units and they are automatically
+  # converted to device pixels for the API to consume where appropriate.
+  #
+  # @note Not possible to make this a mix-in module because Sketchup::InputPoint
+  #   check the type by strict checks instead of an is_a? check.
+  class HighDpiInputPoint < SimpleDelegator
+
+    # @overload initialize
+    #
+    # @overload initialize(inputpoint)
+    #   @param [Sketchup::InputPoint, HighDpiInputPoint] inputpoint
+    def initialize(*args)
+      ip = if args.empty?
+             Sketchup::InputPoint.new
+           elsif args.first.is_a?(Sketchup::InputPoint)
+             args.first
+           else
+             Sketchup::InputPoint.new(*args)
+           end
+      super(ip)
     end
-    super(ip)
-  end
 
-  # @param [HighDpiInputPoint] input_point
-  # @return [HighDpiInputPoint]
-  def copy!(input_point)
-    __getobj__.copy!(input_point.__getobj__)
-    self
-  end
+    # @param [HighDpiInputPoint] input_point
+    # @return [HighDpiInputPoint]
+    def copy!(input_point)
+      __getobj__.copy!(input_point.__getobj__)
+      self
+    end
 
-  # @param [Sketchup::View, HighDpiView]
-  def draw(view)
-    view = view.view if view.is_a?(HighDpiView)
-    __getobj__.draw(view)
-  end
+    # @param [Sketchup::View, HighDpiView]
+    def draw(view)
+      view = view.view if view.is_a?(HighDpiView)
+      __getobj__.draw(view)
+    end
 
-  # @overload pick(view, x, y, inputpoint = nil)
-  #   @param [Sketchup::View, HighDpiView] view
-  #   @param [Integer] x
-  #   @param [Integer] y
-  #   @param [Integer] inputpoint
-  #
-  # @return [Boolean]
-  def pick(*args)
-    args[0] = args[0].view if args[0].is_a?(HighDpiView)
-    args[1] *= DPI.scale_factor # x
-    args[2] *= DPI.scale_factor # y
-    args[3] = args[3].__getobj__ if args.size >= 3 && args[3].is_a?(self.class)
-    __getobj__.pick(*args)
-  end
+    # @overload pick(view, x, y, inputpoint = nil)
+    #   @param [Sketchup::View, HighDpiView] view
+    #   @param [Integer] x
+    #   @param [Integer] y
+    #   @param [Integer] inputpoint
+    #
+    # @return [Boolean]
+    def pick(*args)
+      args[0] = args[0].view if args[0].is_a?(HighDpiView)
+      args[1] *= DPI.scale_factor # x
+      args[2] *= DPI.scale_factor # y
+      args[3] = args[3].__getobj__ if args.size >= 3 && args[3].is_a?(self.class)
+      __getobj__.pick(*args)
+    end
 
-end # class
+  end # class
 end # module
